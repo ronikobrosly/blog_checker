e64ab9273b0ca2bb7fa0c6191b4c08940a1fcd22
Roni Kobrosly <roni.kobrosly@gmail.com>
2022-05-09 09:11:06 -0400

    Update __init__.py

--- blog_checker/__init__.py
+++ blog_checker/__init__.py
@@ -12,6 +12,5 @@ TIMEZONE = pytz.timezone("US/Eastern")
 # Dictionary of blogs/websites and their URLs
 BLOG_URLS = {
     "Sebastian Raschka Blog": "https://sebastianraschka.com/blog/index.html",
-    "Erik Bernhardsson Blog": "https://www.erikbern.com/",
     "Locally Optimistic": "https://locallyoptimistic.com/",
 }
